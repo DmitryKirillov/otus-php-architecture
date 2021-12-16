3439fcbef57d92716bd8a6f4c71670a8efa8e33e
DmitryKirillov <esteps.kirillov@gmail.com>
2021-12-16 14:24:00 +0300

    Add service to controller

--- src/Infrastructure/Http/LeadController.php
+++ src/Infrastructure/Http/LeadController.php
@@ -4,23 +4,36 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Http;
 
+use App\Applicaton\Service\LeadService;
 use App\Infrastructure\Http\Input\CreateLeadRequest;
 use App\Infrastructure\Http\Output\CreateLeadResponse;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Response;
 
 class LeadController extends AbstractFOSRestController
 {
+    private LeadService $leadService;
+
+    /**
+     * @param  LeadService  $leadService
+     */
+    public function __construct(LeadService $leadService)
+    {
+        $this->leadService = $leadService;
+    }
+
     /**
      * @Rest\Post("/api/v1/lead")
      * @ParamConverter("createLeadRequest", converter="fos_rest.request_body")
      * @param  CreateLeadRequest  $createLeadRequest
-     * @return CreateLeadResponse
+     * @return Response
      */
-    public function createLead(CreateLeadRequest $createLeadRequest): CreateLeadResponse
+    public function createLead(CreateLeadRequest $createLeadRequest): Response
     {
-        // todo Реальная отправка лида
-        return new CreateLeadResponse("1", "");
+        $response = $this->leadService->createAndSendLead($createLeadRequest);
+        $view = $this->view($response, 201);
+        return $this->handleView($view);
     }
 }
