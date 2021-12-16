c701383b446525bb6613c5b36753f98c7d7e1cf3
DmitryKirillov <esteps.kirillov@gmail.com>
2021-12-16 14:56:13 +0300

    Add gateway DTOs

--- src/Applicaton/Service/LeadService.php
+++ src/Applicaton/Service/LeadService.php
@@ -6,6 +6,7 @@ namespace App\Applicaton\Service;
 
 use App\Applicaton\DTO\CreateLeadRequest;
 use App\Applicaton\DTO\CreateLeadResponse;
+use App\Applicaton\DTO\SendLeadGatewayRequest;
 use App\Domain\Model\Lead;
 use App\Domain\ValueObject\Name;
 use App\Domain\ValueObject\Phone;
@@ -26,14 +27,40 @@ class LeadService
     public function createAndSendLead(CreateLeadRequest $createLeadRequest): CreateLeadResponse
     {
         try {
-            $lead = new Lead(
-                new Name($createLeadRequest->getName()),
-                new Phone($createLeadRequest->getPhone())
-            );
-            $id = $this->bankGateway->sendLead($lead);
-            return CreateLeadResponse::createWithId($id);
+            $lead = $this->createLead($createLeadRequest);
+            $gatewayRequest = $this->createGatewayRequest($lead);
+            $gatewayResponse = $this->bankGateway->sendLead($gatewayRequest);
+            // todo Добавить ID в лид
+            // todo Сохранить лид в БД
+            return CreateLeadResponse::createWithId($gatewayResponse->getId());
         } catch (\Exception $e) {
             return CreateLeadResponse::createWithError($e->getMessage());
         }
+    }
+
+    /**
+     * @param  CreateLeadRequest  $createLeadRequest
+     * @return Lead
+     */
+    private function createLead(CreateLeadRequest $createLeadRequest): Lead
+    {
+        $lead = new Lead(
+            new Name($createLeadRequest->getName()),
+            new Phone($createLeadRequest->getPhone())
+        );
+        return $lead;
+    }
+
+    /**
+     * @param  Lead  $lead
+     * @return SendLeadGatewayRequest
+     */
+    private function createGatewayRequest(Lead $lead): SendLeadGatewayRequest
+    {
+        $gatewayRequest = new SendLeadGatewayRequest(
+            $lead->getName()->getValue(),
+            $lead->getPhone()->getValue(),
+        );
+        return $gatewayRequest;
     }
 }
--- src/Infrastructure/Gateway/BankGateway.php
+++ src/Infrastructure/Gateway/BankGateway.php
@@ -4,17 +4,18 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Gateway;
 
-use App\Domain\Model\Lead;
+use App\Applicaton\DTO\SendLeadGatewayRequest;
+use App\Applicaton\DTO\SendLeadGatewayResponse;
 
 class BankGateway
 {
-    public function sendLead(Lead $lead): string
+    public function sendLead(SendLeadGatewayRequest $request): SendLeadGatewayResponse
     {
         sleep(2);
         $id = random_int(10_000, 99_999);
         if ($id % 10 <= 2) {
             throw new \Exception("При отправке лида возникла ошибка на стороне банка");
         }
-        return (string)$id;
+        return new SendLeadGatewayResponse((string)$id);
     }
 }
